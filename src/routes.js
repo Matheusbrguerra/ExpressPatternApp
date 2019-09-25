e229fd80a536eb597f9022e7419ed6a50c76c948
MATHEUS BROMONSCHENKEL GUERRA <A06162872@catolica.ubec>
2019-09-25 20:06:19 -0300

    Route has been changed

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ import { Router } from 'express'
 const routes = new Router()
 
 routes.get('/',(req,res)=>{
+    res.json({message:"O index não tem nada :-)"})
+})
+
+routes.get('/index',(req,res)=>{
     res.send("24")
 })
 
